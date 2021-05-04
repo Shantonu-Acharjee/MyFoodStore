d9f965cd1fe890dc96844c0035a24351bf84fae7
shantonuacharjee1 <shantonuacharjee@gmail.com>
2021-05-05 03:41:54 +0600

    My food Store done

---
+++ js/app.js
@@ -0,0 +1,19 @@
+/* ---------------
+Top Projucts Slider Start 
+-----------------*/
+
+$(document).ready(function(){
+    $('.food-slider').slick({
+        autoplay:true,
+        slidesToShow:3,
+        slidesToScroll:1,
+        prevArrow:".prev-btn",
+        nextArrow:".next-btn",
+    });
+});
+
+
+
+/* ---------------
+Top Projucts Slider End 
+-----------------*/
